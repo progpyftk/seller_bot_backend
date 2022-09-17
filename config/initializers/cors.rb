d098f85f3d8adbd10863d11b7cbdc1acc5d75e39
Lorenzo Simonassi <lorenzosimonassi@gmail.com>
2022-09-16 23:39:35 -0300

    t16

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,11 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*', headers: :any, methods: %i[get post]
   end
 end
 
 # For production, use the URL of your production frontend app.
-Rails.application.config.hosts << "https://lorenzosimonassi.gitlab.io/"
