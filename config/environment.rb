ca70ee37d9ae9c74ee91980db50be8873c4730d5
w3villa-sujay <sujay.shukla@w3villa.com>
2023-05-31 01:35:36 +0530

    prodchange

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+# config/environments/<environment>.rb
+Rails.application.configure do
+    # Other configuration settings...
+  
+    config.hosts << "seller-bot-1094006061.us-west-2.elb.amazonaws.com"
+  
+    # Remaining configuration settings...
+  end
+  
