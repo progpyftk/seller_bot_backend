b9c5659e8588e02151d718c6b74195eb99bd9045
Lorenzo Simonassi <lorenzosimonassi@gmail.com>
2023-06-06 10:28:13 -0300

    sidekiq and redis working fine, db migrate on entrypoint added

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+echo "Removing the duplicated pids ..."
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+echo "exec rails db:create ..."
+bundle exec rails db:create
+echo "exec rails db:migrate ..."
+bundle exec rails db:migrate
+echo "Starting app server ..."
 bundle exec rails s -b 0.0.0.0
-
-
-# se eu quiser acessa o rails console eu tenho que rodar esse container serparadamente
-# docker compose run -d -ti app rails console
