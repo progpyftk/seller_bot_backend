b99c7448e46e603d0c81660e8976e91c9d69935a
Lorenzo Simonassi <lorenzosimonassi@gmail.com>
2023-06-08 07:33:54 -0300

    deploying at digitalocean

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -2,14 +2,9 @@
 
 set -e
 
-echo "Removing the duplicated pids ..."
+echo "Removing the duplicated pidssss ..."
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-echo "exec rails db:create ..."
-bundle exec rails db:create
-echo "exec rails db:migrate ..."
-bundle exec rails db:migrate
-echo "Starting app server ..."
 bundle exec rails s -b 0.0.0.0
