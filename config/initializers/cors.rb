37213e274e6347239198bfb024f9baf0d075c6e4
Lorenzo Simonassi <lorenzosimonassi@gmail.com>
2023-07-18 09:09:21 -0300

    working on devise

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource '*', headers: :any, methods: %i[get post]
+    resource(
+       '*', 
+       headers: :any, 
+       methods: [:get, :patch, :put, :delete, :post, :options, :show],
+       expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
+    )
   end
 end
 
