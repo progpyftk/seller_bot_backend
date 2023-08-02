55fb1e3913c875a41ee62a28390dbc566668ecc1
Lorenzo Simonassi <lorenzosimonassi@gmail.com>
2023-08-02 10:40:10 -0300

    tudo rodando certo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://sellerbot.com.br', 'http://api.sellerbot.com.br',  'sellerbot.com.br', 'api.sellerbot.com.br', 'http://138.197.230.3',  '*'
+    origins 'http://sellerbot.com.br', 'http://api.sellerbot.com.br',  'sellerbot.com.br', 'api.sellerbot.com.br', '*'
 
     resource(
        '*', 
