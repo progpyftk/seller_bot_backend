ee33e16891a6cdd5a33035b0f8bb757abe7aa81c
Lorenzo Simonassi <lorenzo@Desktop-Casa>
2023-12-10 08:36:20 -0300

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,6 +86,6 @@ Rails.application.configure do
   config.hosts << "sellerbot.com.br"
   config.hosts << "api.sellerbot.com.br"
   config.hosts << "oyster-app-vu64f.ondigitalocean.app"
-  onfig.hosts << "sea-turtle-app-ehicf.ondigitalocean.app"
+  config.hosts << "sea-turtle-app-ehicf.ondigitalocean.app"
 
 end
