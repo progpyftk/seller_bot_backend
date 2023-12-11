2def78471c996513cbf4ab6dde24aa0219c24e37
Lorenzo Simonassi <lorenzo@Desktop-Casa>
2023-12-11 11:44:45 -0300

    added config hosts to production.rb for actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,9 +51,14 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://oyster-app-vu64f.ondigitalocean.app/cable'
+  config.action_cable.allowed_request_origins = [
+    'https://oyster-app-vu64f.ondigitalocean.app',
+    /https:\/\/oyster-app-vu64f\.ondigitalocean\.app/,
+    'https://sea-turtle-app-ehicf.ondigitalocean.app/',
+    /https:\/\/sea-turtle-app-ehicf.\.ondigitalocean\.app/,
 
+  ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
